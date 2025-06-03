384add98356433d32b702f573a60652ae3f46c2f
Borja725 <borja105702@gmail.com>
2025-06-03 20:36:29 +0200

    user table defined

--- backend/src/Entity/User.php
+++ backend/src/Entity/User.php
@@ -9,6 +9,7 @@ use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
+#[ORM\Table(name: "users")]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
